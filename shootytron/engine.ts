e52e961b0aa11d4e8c7da3d00a08adc2b53436b8
Daniel Cassidy <mail@danielcassidy.me.uk>
2013-06-10 14:43:13 +0100

    Add engine module with Clock interface.

---
+++ shootytron/engine.ts
@@ -0,0 +1,10 @@
+﻿/** Interface for a clock function that returns a continuously updating 
+ * count of milliseconds since some arbitrary epoch.
+ *
+ * The clock should be continuous and consistent, but need not be in any
+ * particular time zone or relative to any particular epoch.
+ *
+ * Date.now is suitable for use as an implementation of this interface. */
+export interface Clock {
+    (): number;
+}
